a9fd55101928f3653cc1cd589b29c2c0ffbd00c2
unknown <luisportelacoelho@gmail.com>
2018-06-05 00:36:53 +0100

    Add more test in order to cover all the situations.

--- exercises/timeConversion/time_conversion.py
+++ exercises/timeConversion/time_conversion.py
@@ -5,7 +5,9 @@ def time_conversion(time):
     second = time[6:8]
     time_format = time[8:]
 
-    if time_format == "PM":
+    if time_format == "PM" and hour != "12":
         hour = str(int(hour) + 12)
+    elif time_format == "AM" and hour == "12":
+        hour = "00"
 
     return hour + ":" + minute + ":" + second
--- exercises/timeConversion/time_conversion_test.py
+++ exercises/timeConversion/time_conversion_test.py
@@ -6,6 +6,11 @@ class Test(TestCase):
     def test_time_conversion(self):
         self.assertEqual(time_conversion("07:05:45PM"), "19:05:45")
 
+    def test_time_conversion2(self):
+        self.assertEqual(time_conversion("12:40:22AM"), "00:40:22")
+
+    def test_time_conversion3(self):
+        self.assertEqual(time_conversion("12:45:54PM"), "12:45:54")
 
 if __name__ == '__main__':
     main()
