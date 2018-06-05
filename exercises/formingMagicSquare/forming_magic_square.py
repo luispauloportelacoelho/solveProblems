ea6fe53ef10326896d1f46923161543f3354902c
unknown <luisportelacoelho@gmail.com>
2018-06-05 01:59:49 +0100

    Update: calculate the sum of rows and columns.

--- exercises/formingMagicSquare/forming_magic_square.py
+++ exercises/formingMagicSquare/forming_magic_square.py
@@ -2,7 +2,28 @@ def forming_magic_square(s):
 
     dim = len(s)
 
-    return dim
+    count = 0
+    saveResult = []
+    for i in range(dim):
+        t = 0
+        for j in range(dim):
+            t += s[i][j]
+        saveResult.append(t)
+
+    t = 0
+    for i in range(dim):
+        t += s[i][i]
+
+    saveResult.append(t)
+
+    for i in range(dim):
+        t = 0
+        for j in range(dim):
+            t += s[j][i]
+
+        saveResult.append(t)
+
+    return saveResult
 
 
 print(forming_magic_square([[4, 9, 2], [3, 5, 7], [8, 1, 5]]))
