cfc2ff8a7ae8f50b5426d63080b9dd799b6de406
unknown <luisportelacoelho@gmail.com>
2018-06-12 00:16:00 +0100

    Update.

---
+++ exercises/fraudulentActivityNotifications/fraudulent_activity_notifications.py
@@ -0,0 +1,29 @@
+def activityNotifications(expenditure, d):
+    numberAlerts = 0
+
+    if (d // 2 == 0):
+        typeOfMedian = True
+        position = d / 2
+    else:
+        typeOfMedian = False
+        position = (d / 2) - 0.5
+
+    size = len(expenditure) - 1
+
+    for x in range(d, size):
+        median = calculateMedian(expenditure[(x - d):(x - 1)], typeOfMedian, int(position))
+
+        if expenditure[x] >= (median * 2):
+            numberAlerts += 1
+
+    return numberAlerts
+
+
+def calculateMedian(subArray, typeOfMedian, position):
+
+    sortSubArray = sorted(subArray)
+
+    if typeOfMedian:
+        return ((sortSubArray[position] + sortSubArray[position - 1]) / 2)
+    else:
+        return sortSubArray[position]
---
+++ exercises/fraudulentActivityNotifications/fraudulent_activity_notifications_test.py
@@ -0,0 +1,24 @@
+from unittest import TestCase, main
+from fraudulent_activity_notifications import activityNotifications, calculateMedian
+
+
+class Test(TestCase):
+    def test_input0(self):
+        self.assertEqual(activityNotifications([2, 3, 4, 2, 3, 6, 8, 4, 5], 5),
+                         2)
+
+    def test_input1(self):
+        self.assertEqual(activityNotifications([1, 2, 3, 4, 4], 4), 0)
+
+    def test_input2(self):
+        self.assertEqual(activityNotifications([10, 20, 30, 40, 50], 3), 1)
+
+    def test_median_odd(self):
+        self.assertEqual(calculateMedian([10, 20, 30], False, 1), 20)
+
+    def test_median_even(self):
+        self.assertEqual(calculateMedian([1, 2, 3, 4, 5, 6, 8, 9], True, 4), 4.5)
+
+
+if __name__ == '__main__':
+     main()
