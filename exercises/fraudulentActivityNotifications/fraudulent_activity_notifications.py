ecf3d769875ab7c0fd13941c6a597f7395fdf384
unknown <luisportelacoelho@gmail.com>
2018-06-16 19:10:18 +0100

    Update: first steps with heap.

--- exercises/fraudulentActivityNotifications/fraudulent_activity_notifications.py
+++ exercises/fraudulentActivityNotifications/fraudulent_activity_notifications.py
@@ -1,3 +1,5 @@
+import heapq
+
 def activityNotifications(expenditure, d):
     numberAlerts = 0
 
@@ -21,9 +23,30 @@ def activityNotifications(expenditure, d):
 
 def calculateMedian(subArray, typeOfMedian, position):
 
-    sortSubArray = sorted(subArray)
+    #sortSubArray = sorted(subArray)
+    sortSubArray = sortHeap(subArray)
+    print(sortSubArray)
 
     if typeOfMedian:
         return ((sortSubArray[position] + sortSubArray[position - 1]) / 2)
     else:
         return sortSubArray[position]
+
+
+def sortHeap(subArray):
+
+    sizeA = len(subArray)
+
+    heap = []
+    for x in range(0, sizeA):
+        heapq.heappush(heap, subArray[x])
+
+    heapq.heapify(heap)
+    print(subArray)
+    print(heap)
+    print(heap[1])
+
+
+    return heap
+
+sortHeap([20, 3, 4, 2, 3, 6, 8, 4, 5])
--- exercises/fraudulentActivityNotifications/fraudulent_activity_notifications_test.py
+++ exercises/fraudulentActivityNotifications/fraudulent_activity_notifications_test.py
@@ -1,6 +1,7 @@
 from unittest import TestCase, main
 from fraudulent_activity_notifications import activityNotifications
-from fraudulent_activity_notifications import calculateMedian                                              calculateMedian
+from fraudulent_activity_notifications import calculateMedian
+from fraudulent_activity_notifications import sortHeap
 
 
 class Test(TestCase):
@@ -19,6 +20,18 @@ class Test(TestCase):
 
     def test_median_even(self):
         self.assertEqual(calculateMedian([1, 2, 3, 4, 5, 6, 8, 9], True, 4), 4.5)
+
+    def test_sort_heap1(self):
+        self.assertEqual(sortHeap([2, 4, 2, 5, 1]), [1, 2, 2, 5, 4])
+
+    def test_sort_heap2(self):
+        self.assertEqual(sortHeap([1, 2, 3, 4]), [1, 2, 3, 4])
+
+    def test_sort_heap3(self):
+        self.assertEqual(sortHeap([2, 3, 4, 4]), [2, 3, 4, 4])
+
+    def test_sort_heap4(self):
+        self.assertEqual(sortHeap([2, 3, 4, 2, 3]), [2, 2, 4, 3, 3])
 
 
 if __name__ == '__main__':
