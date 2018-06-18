48549172932ef01ce9c36d414ab15b10d300e9a2
unknown <luisportelacoelho@gmail.com>
2018-06-18 02:01:33 +0100

    Refactoring code for optimization.

--- exercises/climbingTheLeaderboard/climbing_the_leaderboard.py
+++ exercises/climbingTheLeaderboard/climbing_the_leaderboard.py
@@ -1,40 +1,14 @@
 def climbingLeaderboard(scores, alice):
     """Climbing the leadboard."""
-    allPositions, numberScores = classify_scores(scores)
 
-    playerPosition = []
+    leaderboard = sorted(set(scores), reverse = True)
+    sizeLeaderboard = len(leaderboard)
 
-    numberGames = len(alice)
+    result = []
 
-    for x in range(numberGames):
+    for a in alice:
+        while (sizeLeaderboard > 0) and (a >= leaderboard[sizeLeaderboard-1]):
+            sizeLeaderboard -= 1
+        result.append(sizeLeaderboard+1)
 
-        if alice[x] in scores:
-            value_index = scores.index(alice[x])
-            playerPosition.append(allPositions[value_index])
-        else:
-            if alice[x] < scores[-1]:
-                playerPosition.append(allPositions[-1] + 1)
-            elif alice[x] > scores[0]:
-                playerPosition.append(1)
-            else:
-                vector = [i for i in scores if i > alice[x]]
-                sizeVector = len(vector)
-                playerPosition.append(allPositions[sizeVector])
-
-    return playerPosition
-
-
-def classify_scores(scores):
-    """Give a classification for all scores."""
-
-    numberScores = len(scores)
-
-    position = [1]
-
-    for x in range(1, numberScores):
-        if scores[x-1] == scores[x]:
-            position.append(position[x-1])
-        else:
-            position.append(position[x-1] + 1)
-
-    return position, numberScores
+    return result  
--- exercises/climbingTheLeaderboard/climbing_the_leaderboard_test.py
+++ exercises/climbingTheLeaderboard/climbing_the_leaderboard_test.py
@@ -1,6 +1,5 @@
 from unittest import TestCase, main
 from climbing_the_leaderboard import climbingLeaderboard
-from climbing_the_leaderboard import classify_scores
 
 
 class Test(TestCase):
@@ -9,11 +8,6 @@ class Test(TestCase):
         """Test the climbing_leader_board with the given example."""
         self.assertEqual(climbingLeaderboard([100, 100, 50, 40, 40, 20, 10],
                                              [5, 25, 50, 120]), [6, 4, 2, 1])
-
-    def test_classify_scores(self):
-        """Classification for scores."""
-        self.assertEqual(classify_scores([100, 100, 50, 40, 40, 30]),
-                         ([1, 1, 2, 3, 3, 4], 6))
 
 
 if __name__ == '__main__':
