fccb060d489427f08070cca2cdc3a16eb5df3dc4
unknown <luisportelacoelho@gmail.com>
2018-06-26 00:20:41 +0100

    WIP for queens attack.

--- exercises/queensAttack2/queens_attack2.py
+++ exercises/queensAttack2/queens_attack2.py
@@ -2,4 +2,14 @@ def queensAttack(n, k, r_q, c_q, obstacles):
 
     totalPosition = n * n
 
-    return 9
+    verticalPos = n - r_q
+    verticalNeg = r_q - 1
+    lateralPos = n - c_q
+    lateralNeg = c_q - 1
+
+    for x in range(r_q, n):
+        
+
+
+    moves = freeMoves - obstaclesMoves
+    return moves
