393956ff6cbcf16464a593afee89e8c08bf73dba
unknown <luisportelacoelho@gmail.com>
2018-07-03 21:26:02 +0100

    Add validation in order to count the number of times that a word appears.

--- exercises/hashTablesRansomNote/hash_tables_ransom_note.py
+++ exercises/hashTablesRansomNote/hash_tables_ransom_note.py
@@ -1,14 +1,19 @@
 def checkMagazine(magazine, note):
 
     repeat = {}
+    for word in magazine:
+        if not(word in repeat):
+            repeat[word] = 1
+        else:
+            repeat[word] += 1
 
     answer = 'Yes'
 
     for i in range(len(note)):
-        if not(note[i] in magazine) or (note[i] in repeat):
+        if not(note[i] in magazine) or (repeat[note[i]] == 0):
             answer = 'No'
             break
         else:
-            repeat.update({note[i]: 1})
+            repeat[note[i]] -= 1
 
     print(answer)
