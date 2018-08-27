467f7e064171ef719d8ceb93f5843e6d3f77162a
unknown <luisportelacoelho@gmail.com>
2018-08-28 00:08:42 +0100

    Update using min heap and max heap.

--- exercises/heapFindRunningMedian/heap_find_running_median.py
+++ exercises/heapFindRunningMedian/heap_find_running_median.py
@@ -1,32 +1,66 @@
-import heapq
-
 def runningMedian(a):
-
+    #
+    # Write your code here.
+    #
     lenA = len(a)
 
     medianArr = []
 
-    selectValue = []
+    minHeap = []
+    maxHeap = []
+    minHeapSize = 0
+    maxHeapSize = 0
 
     countHeapSize = 0
 
-    for x in a:
-        heapq.heappush(selectValue, x)
-        heapq.heapify(selectValue)
-
-        print(selectValue)
-
-        countHeapSize += 1
-
-        calc = int(countHeapSize / 2)
-
-        if countHeapSize % 2 == 0:
-            medianVal = (selectValue[calc] + selectValue[calc - 1])/2
-            medianArr.append(medianVal)
+    for x in range(lenA):
+        print("#####")
+        if x == 0:
+            heapq.heappush(minHeap, a[x])
+            calc = a[x]
+            minHeapSize += 1
         else:
-            medianArr.append(selectValue[calc])
+            if a[x] < calc:
+                heapq.heappush(maxHeap, -1 * a[x])
+                maxHeapSize += 1
+            else:
+                heapq.heappush(minHeap, a[x])
+                minHeapSize += 1
+
+            differenceHeapSize = abs(minHeapSize - maxHeapSize)
+
+            print("======")
+            print(differenceHeapSize)
+
+            if differenceHeapSize == 2 and minHeapSize < maxHeapSize:
+                heapq.heappush(minHeap, -1 * heapq.heappop(maxHeap))
+
+                maxHeapSize -= 1
+                minHeapSize += 1
+
+            elif differenceHeapSize == 2 and minHeapSize > maxHeapSize:
+                heapq.heappush(maxHeap, -1 * heapq.heappop(minHeap))
+
+                maxHeapSize += 1
+                minHeapSize -= 1
+
+            differenceHeapSize = abs(minHeapSize - maxHeapSize)
+            if differenceHeapSize == 0:
+                calc = (-1 * maxHeap[0] + minHeap[0]) / 2
+                print("----------------")
+                print(-1 * maxHeap[0])
+                print(minHeap[0])
+            elif minHeapSize > maxHeapSize:
+                print("^^^^^^^^^^^^^^^^")
+                calc = minHeap[0]
+            elif minHeapSize < maxHeapSize:
+                print("~~~~~~~~~~~~~~~~")
+                calc = -1 * maxHeap[0]
+
+        print(calc)
+        print(minHeap)
+        print(maxHeap)
+
+        medianArr.append(calc)
 
     return medianArr
-
-
-    return 1
