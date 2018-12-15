04a9ca84f90df28577843310f9351a00774e0316
unknown <luisportelacoelho@gmail.com>
2018-12-15 17:14:21 +0000

    Exercise: Fraudulent Notifications. Update code and test files.

--- exercises/fraudulentActivityNotifications/fraudulent_activity_notifications.py
+++ exercises/fraudulentActivityNotifications/fraudulent_activity_notifications.py
@@ -1,9 +1,14 @@
+import math
+import os
+import random
+import re
+import sys
 import heapq
 
 # Complete the activityNotifications function below.
 def activityNotifications(expenditure, d):
 
-    choose = int(d / 2)
+    choose = round(d / 2)
 
     lenExpenditure = len(expenditure)
 
@@ -16,13 +21,16 @@ def activityNotifications(expenditure, d):
     maxHeap = [x * (-1) for x in maxHeap]
     maxHeapSize = len(maxHeap)
 
-    heapq.heapify(minHeap)
-    heapq.heapify(maxHeap)
+    #heapq.heapify(minHeap)
+
+    maxHeap.sort()
 
     if minHeapSize == maxHeapSize:
         firstMedian = (minHeap[0] - maxHeap[0]) / 2
-    else:
+    elif minHeapSize > maxHeapSize:
         firstMedian = minHeap[0]
+    elif minHeapSize < maxHeapSize:
+        firstMedian = -1 * maxHeap[0]
 
     notifications = 0
     countCycles = 0
@@ -31,8 +39,10 @@ def activityNotifications(expenditure, d):
         notifications += 1
 
     for x in range(d + 1, lenExpenditure):
+
         valueToRemove = expenditure[countCycles]
         countCycles += 1
+
         if minHeap[0] > valueToRemove:
 
             i = maxHeap.index(-1 * valueToRemove)
@@ -42,7 +52,7 @@ def activityNotifications(expenditure, d):
             maxHeap.pop()
             maxHeapSize -= 1
 
-            if i < len(maxHeap):
+            if i < maxHeapSize:
                 heapq._siftup(maxHeap, i)
                 heapq._siftdown(maxHeap, 0, i)
         else:
@@ -53,40 +63,48 @@ def activityNotifications(expenditure, d):
             minHeap.pop()
             minHeapSize -= 1
 
-            if i < len(minHeap):
+            if i < minHeapSize:
                 heapq._siftup(minHeap, i)
                 heapq._siftdown(minHeap, 0, i)
 
-        if minHeap[0] > expenditure[x-1]:
-            heapq.heappush(maxHeap, -1 * expenditure[x-1])
+        if minHeap[0] > expenditure[x]:
+            heapq.heappush(maxHeap, -1 * expenditure[x])
             maxHeapSize += 1
         else:
-            heapq.heappush(minHeap, expenditure[x-1])
+            heapq.heappush(minHeap, expenditure[x])
             minHeapSize += 1
 
-        #print('before update')
-        #print(minHeap)
-        #print(minHeapSize)
-        #print(maxHeap)
-        #print(maxHeapSize)
         if abs(minHeapSize - maxHeapSize) >= 2:
             if minHeapSize > maxHeapSize:
-                #heapq.heappush(maxHeap, -1 * heapq.heapreplace(minHeap, a[x]))
                 heapq.heappush(maxHeap, -1 * heapq.heappop(minHeap))
                 minHeapSize -= 1
                 maxHeapSize += 1
             else:
-                #heapq.heappush(minHeap, -1 * heapq.heapreplace(maxHeap, -1 * a[x]))
                 heapq.heappush(minHeap, -1 * heapq.heappop(maxHeap))
                 minHeapSize += 1
                 maxHeapSize -= 1
-        #print('after update')
-        #print(minHeap)
-        #print(maxHeap)
 
-        if ((d % 2) == 0 and (expenditure[d] >= (minHeap[0] - maxHeap[0]))) or expenditure[d] >= 2 * minHeap[0]:
+        if minHeapSize == maxHeapSize and (expenditure[x] >= (minHeap[0] - maxHeap[0])):
+            notifications += 1
+        elif minHeapSize > maxHeapSize and expenditure[x] >= 2 * minHeap[0]:
+            notifications += 1
+        elif minHeapSize < maxHeapSize and expenditure[x] >= -2 * maxHeap[0]:
             notifications += 1
 
-    print(notifications)
-
     return notifications
+
+
+fptr = open('/Users/Luís Coelho/Desktop/Luis Coelho_20180321/Luís Coelho/Git/solveProblems/exercises/fraudulentActivityNotifications/fraudulent.txt', 'r')
+n = (fptr.read().split("\n"))
+v = n[0].split(" ")
+v1 = n[1].split(" ")
+v = list(map(int, v))
+v1 = list(map(int, v1))
+
+print(activityNotifications(v1, v[1]))
+
+#n = int(nd[0])
+#d = int(nd[1])
+
+#expenditure = list(map(int, input().rstrip().split()))
+fptr.close()
