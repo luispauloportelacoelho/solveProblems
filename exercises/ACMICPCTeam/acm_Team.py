3fda5bcfe34e75ed0c1164466420d991112e49b3
Luís Coelho <luisportelacoelho@gmail.com>
2019-05-01 16:38:43 +0100

    Simplify the code.

--- exercises/ACMICPCTeam/acm_Team.py
+++ exercises/ACMICPCTeam/acm_Team.py
@@ -1,12 +1,14 @@
+from itertools import combinations
+
 def acmTeam(topic):
 
     positions = list(range(0, len(topic)))
 
     max_value = 0
+    number_of_max = 0
 
-    save_values = []
+    unorderedPairGenerator = combinations(positions, 2)
 
-    unorderedPairGenerator = ([x, y] for x in positions for y in positions if y > x)
     for pair in unorderedPairGenerator:
 
         list1 = list(topic[pair[0]])
@@ -17,18 +19,13 @@ def acmTeam(topic):
         pair_value = 0
 
         for x in range(list1_size):
-            if list1[x] == '1' or list2[x] == '1':
-
+            if (list1[x] == '1') or (list2[x] == '1'):
                 pair_value += 1
 
-        save_values.append(pair_value)
+        if pair_value == max_value:
+            number_of_max += 1
+        elif pair_value > max_value:
+            max_value = pair_value
+            number_of_max = 1
 
-    result = []
-
-    max_value = max(save_values)
-
-    result.append(max_value)
-
-    result.append(save_values.count(max_value))
-
-    return result
+    return [max_value, number_of_max]
